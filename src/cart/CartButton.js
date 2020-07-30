a0803d3e719e3ba21fc2a0ada870b648e559e200
Daniel Montoya <dsmontoyam@gmail.com>
2020-07-30 16:58:40 -0500

    show counter if value is greater than 0.

--- src/cart/CartButton.js
+++ src/cart/CartButton.js
@@ -4,12 +4,22 @@ import "./CartButton.css"
 function CartButton(props) {
   return (
     <div className="CartButton">
-      <div className="CartButton-counter">
-        {props.numberOfProducts}
-      </div>
+      <Counter value={props.numberOfProducts}/>
       <img src={icon} alt="cart button" className="CartButton-image"></img>
     </div>
   );
+}
+
+function Counter(props) {
+  const value = props.value
+  if (value > 0) {
+    return (
+      <div className="CartButton-counter">
+        {value}
+      </div>
+    )
+  }
+  return null
 }
 
 export default CartButton;
