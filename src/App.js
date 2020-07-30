9fbdc9fd62badb82c78beb322453d8e71e9f879b
Daniel Montoya <dsmontoyam@gmail.com>
2020-07-30 10:36:24 -0500

    fix header

--- src/App.js
+++ src/App.js
@@ -15,23 +15,26 @@ import Home from "./home/Home.js";
 function App() {
   const [products, setProducts] = useState([
     {id: 1, name:"Summer", image: "summer-pizza.png", description: "Shrimp, Red Capsicum, Green Capsicum, Onion, Chilli flakes, Lemon Pepper, Mozzarella, finished with Aioli", price: 10},
-    {id: 2, name:"Pepperoni", image: "pepperoni-pizza.png", description: "Extra-virgin olive oil, garlic, mozzarella cheese, onions, mushrooms, green olives, black olives, fresh tomatoes.", price: 7},
+    {id: 2, name:"Pepperoni", image: "pepperoni-pizza.png", description: "Olive oil, garlic, mozzarella cheese, onions, mushrooms, green olives, black olives, fresh tomatoes.", price: 7},
   ]);
   return (
     <div className="App">
-      <header>
-        lol
-      </header>
       <Router>
-        <div>
-          <nav>
+        <header className="Header col-12">
+          <div className=" Header-title col-3">
+            <Link to="/">
+              <h1>Alfredo's Pizza Cafe</h1>
+            </Link>
+          </div>
+          <div className="col-7"></div>
+          <nav className="col-2">
             <ul>
-              <li><Link to="/">Alfredo's Pizza Cafe</Link></li>
-              <li><Link to="/history">History</Link></li>
-              <li><Link to="/cart"><CartButton></CartButton></Link></li>
+              <li className="Header-item Header-history col-7"><Link to="/history">History</Link></li>
+              <li className="Header-item Header-cart col-5"><Link to="/cart"><CartButton></CartButton></Link></li>
             </ul>
           </nav>
-
+        </header>
+        <div>
           <Switch>
             <Route path="/cart">
               <Cart />
--- src/cart/CartButton.js
+++ src/cart/CartButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import icon from "./cart-icon.svg";
+import "./CartButton.css"
 function CartButton() {
   return (
     <div className="CartButton">
-      <img src={icon} alt="cart button"></img>
+      <img src={icon} alt="cart button" className="CartButton-image"></img>
     </div>
   );
 }
