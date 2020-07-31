d95b699442e5f65745c67f1a949e9f4592a82f05
Daniel Montoya <dsmontoyam@gmail.com>
2020-07-31 14:19:52 -0500

    fix rendering loop error

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ProductList from '../products/ProductList';
 import "./Cart.css"
 
 function Cart(props) {
   const products = props.products.slice();
-  const content = (products.length > 0 ? <ProductList display={"inline"} products={groupProductsByID()}/>:<EmptyCart/>);
-
-  function groupProductsByID() {
-    let groupedProducts = [];
+  var cartInfo = getCartInfo();
+  const [groupedProducts, setGroupedProducts] = useState(cartInfo.products);
+  var content = (products.length > 0 ? <ProductList display={"inline"} products={groupedProducts}/>:<EmptyCart/>);
+  
+  function getCartInfo() {
+    let gp = [];
     let hash = {};
     products.forEach(function(product) {
       const id = product.id
       if (!(id in hash)) {
         product.amount = 1;
-        hash[id] = groupedProducts.length;
-        groupedProducts.push(product);
+        hash[id] = gp.length;
+        gp.push(product);
       } else {
-        const idx = hash[id]
-        groupedProducts[idx].amount++
-      }
+          const idx = hash[id]
+          gp[idx].amount++
+        }
     });
-    return groupedProducts;
+    console.log("gp",gp);
+    return {products: gp}
   }
-  
+    
   return (
     <div className="Container">
-      {content}
+     {content}
     </div>
   );
 }
@@ -36,6 +39,15 @@ function EmptyCart() {
       Your cart is empty!
     </div>
   )
+}
+
+function PurchaseButton(props) {
+  if (props.total > 0) {
+    return(
+      <button>Check out (${props.total})</button>
+    )
+  }
+  return null
 }
 
 export default Cart;
